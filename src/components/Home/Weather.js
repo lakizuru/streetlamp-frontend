84224e3750233378f63e239468352edc906bdcf3
Lakisuru Semasinghe <semasinghels@gmail.com>
2021-09-28 22:32:20 +0530

    weather loading error fixed

--- src/components/Home/Weather.js
+++ src/components/Home/Weather.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import moment from 'moment';
+import moment from "moment";
 
 export default function Weather() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fectchData = async () => {
@@ -14,11 +15,12 @@ export default function Weather() {
       });
 
       await fetch(
-        "https://api.openweathermap.org/data/2.5/weather/?lat=6.92&lon=79.865&units=metric&APPID=2a23a8b30b6209b6b64227d4960dcd9b"
+        `https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
       )
         .then((res) => res.json())
         .then((result) => {
           setData(result);
+          setIsLoading(false);
         });
     };
 
@@ -26,27 +28,41 @@ export default function Weather() {
     console.log(data);
   }, [lat, long]);
 
-  return (
-    <div className="main">
-    <div className="row">
-    <div className="col"><p className="header">{data.name}</p></div>
-    <div className="col"><p className="description">{data.weather[0].main}</p></div>
-    </div>  
-      <div className="flex">
-        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-        
-      </div>
+  if (typeof data.main == 'undefined') {
+    return <div>Loading</div>;
+  } else {
+    return (
+      <div className="main">
+        <div className="row">
+          <div className="col">
+            <p className="header">{data.name}</p>
+          </div>
+          <div className="col">
+            <p className="description">{data.weather[0].main}</p>
+          </div>
+        </div>
+        <div className="flex">
+          <p className="day">
+            {moment().format("dddd")}, <span>{moment().format("LL")}</span>
+          </p>
+        </div>
 
-      <div className="flex">
-        <p className="temp">Temprature: {data.main.temp} &deg;C</p>
-        <p className="temp">Humidity: {data.main.humidity} %</p>
-      </div>
+        <div className="flex">
+          <p className="temp">Temprature: {data.main.temp} &deg;C</p>
+          <p className="temp">Humidity: {data.main.humidity} %</p>
+        </div>
 
-      <div className="flex">
-        <p className="sunrise-sunset">Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-        <p className="sunrise-sunset">Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+        <div className="flex">
+          <p className="sunrise-sunset">
+            Sunrise:{" "}
+            {new Date(data.sys.sunrise * 1000).toLocaleTimeString("en-IN")}
+          </p>
+          <p className="sunrise-sunset">
+            Sunset:{" "}
+            {new Date(data.sys.sunset * 1000).toLocaleTimeString("en-IN")}
+          </p>
+        </div>
       </div>
-    
-  </div>
-  );
+    );
+  }
 }
